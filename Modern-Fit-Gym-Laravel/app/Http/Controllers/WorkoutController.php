1344eefb1c7df90e1edfe1bf045b7fa2ee550d68
N00rAhmed <tronn232003@gmail.com>
2023-12-05 20:20:32 +0000

    added code for controllers

---
+++ Modern-Fit-Gym-Laravel/app/Http/Controllers/WorkoutController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App;
+
+class Workout
+{
+    private $WorkoutID = 2;
+    private $StaffID = 3;
+    private $MemberID = 4;
+    private $ExcerciseName = "run";
+    private $ExcerciseType = "excercise type";
+    private $Description = "description";
+    private $Amount = 21;
+
+    // private function workoutController()
+    // {
+    //     // $WorkoutID = 2;
+    //     // $StaffID = 3;
+    //     // $MemberID = 4;
+    //     // $ExcerciseName = "run";
+    //     // $ExcerciseType = "excercise type";
+    //     // $Description = "description";
+    //     // $Amount = 21;
+    // }
+
+    public function getWorkoutID()
+    {
+        return $WorkoutID;
+    }
+    public function getStaffID(){
+        return $StaffID;
+    }
+    public function getMemberID(){
+        return $MemberID;
+    }
+    public function getExcerciseName(){
+        return $ExcerciseName;
+    }
+    public function getExcerciseType(){
+        return $ExcerciseType;
+    }
+    public function getDescription(){
+        return $Description;
+    }
+    public function getAmount(){
+        return $Amount;
+    }
+
+    
+    public function setWorkoutID($workout_id){
+        $WorkoutID = $workout_id;
+    }
+    public function setStaffID($staff_id){
+         $StaffID = $staff_id;
+    }
+    public function setMemberID($member_id){
+        $MemberID = $member_id;
+    }
+    public function setExcerciseName($excercise_name){
+        $ExcerciseName = $excercise_name;
+    }
+    public function setExcerciseType($excercise_type){
+        $ExcerciseType = $excercise_type;
+    }
+    public function setDescription($description){
+        $Description = $description;
+    }
+    public function setAmount($amount){
+        $Amount = $amount;
+    }
+
+
+    public function SearchFunction(){
+
+    }
+
+    
+}
