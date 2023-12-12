6aca572769ea72f95ab8e2ef7a2ca9acb2f1f2fb
Montgomery18 <c2026842@hallam.shu.ac.uk>
2023-12-12 19:51:10 +0000

    Updated registration model/controller code, unique code is now generated for users

--- Modern-Fit-Gym-Laravel/resources/views/diary.blade.php
+++ Modern-Fit-Gym-Laravel/resources/views/diary.blade.php
@@ -1,102 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-
-<style>
-table {
-    
-  font-family: arial, sans-serif;
-  /* border-collapse: collapse;
-  width: 100%; */
-}
-
-td, th {
-
-  border: 1px solid #000000;
-  /* text-align: left;
-  padding: 8px; */
-}
-
-tr:nth-child(even) {
-
-  /* background-color: #dddddd; */
-}
-</style>
-
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="../Styles/diary.css"> -->
     <link rel="stylesheet" href="{{ asset('../css/diary.css') }}">
-
     <title>My diary</title>
 </head>
-<body>
-@extends('nav')
-    @section('content')
 
+<body>
+    @extends('nav')
+    @section('content')
     <main>
         <h2>Diary Entry</h2>
         <div class="input">
             <form action="{{ route('diary.submit') }}" method="post">
-            @csrf <!-- {{ csrf_field() }} -->
-
+                @csrf
                 <input type="text" name="date" id="date" placeholder="Date" />
-                <input type="text" name="calorie_intake" id="calorie_intake" placeholder="Calorie Intake"/>
-                <input type="text" name="supplement_intake" id="supplement_intake" placeholder="Supplement Intake"/>
-                <input type="text" name="exercise" id="exercise" placeholder="Exercise"/>
-                <input type="text" name="daily_duration" id="daily_duration" placeholder="Daily Duration"/>
-                <input type="text" name="notes" id="notes" placeholder="Notes"/>
-        </div>
-        <br />
-        <div class="buttons">
-            <button type="submit">Add</button>
+                <input type="text" name="calorie_intake" id="calorie_intake" placeholder="Calorie Intake" />
+                <input type="text" name="supplement_intake" id="supplement_intake" placeholder="Supplement Intake" />
+                <input type="text" name="exercise" id="exercise" placeholder="Exercise" />
+                <input type="text" name="daily_duration" id="daily_duration" placeholder="Daily Duration" />
+                <input type="text" name="notes" id="notes" placeholder="Notes" />
+                <button type="submit" class="coloured-button">Add</button>
             </form>
-
         </div>
-
         <table>
-  <thead>
-    <tr>
-      <th>Diary_ID</th>
-      <th>Member_ID</th>
-      <th>Date</th>
-      <th>Calorie_Intake</th>
-      <th>Supplement_Intake</th>
-      <th>Exercise</th>
-      <th>Daily_Duration</th>
-      <th>Notes</th>
-    </tr>
-  </thead>
-  <tbody>
-  @if(is_array($data) || is_object($data))
-        @foreach ($data as $data1)
-            <tr>
-            <td>{{ $data1->Diary_ID }}</td>
-        <td>{{ $data1->Member_ID }}</td>
-        <td>{{ $data1->Date }}</td>
-        <td>{{ $data1->Calorie_Intake }}</td>
-        <td>{{ $data1->Supplement_Intake }}</td>
-        <td>{{ $data1->Exercise }}</td>
-        <td>{{ $data1->Daily_Duration }}</td>
-        <td>{{ $data1->Notes }}</td>
-                <!-- Rest of your table data -->
-            </tr>
-        @endforeach
-    @else
-        <tr>
-            <td colspan="8">No data available</td>
-        </tr>
-    @endif
-  </tbody>
-</table>
-
-
+            <thead>
+                <tr>
+                    <th>Diary_ID</th>
+                    <th>Member_ID</th>
+                    <th>Date</th>
+                    <th>Calorie_Intake</th>
+                    <th>Supplement_Intake</th>
+                    <th>Exercise</th>
+                    <th>Daily_Duration</th>
+                    <th>Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if(is_array($data) || is_object($data))
+                @foreach ($data as $data1)
+                <tr>
+                    <td>{{ $data1->Diary_ID }}</td>
+                    <td>{{ $data1->Member_ID }}</td>
+                    <td>{{ $data1->Date }}</td>
+                    <td>{{ $data1->Calorie_Intake }}</td>
+                    <td>{{ $data1->Supplement_Intake }}</td>
+                    <td>{{ $data1->Exercise }}</td>
+                    <td>{{ $data1->Daily_Duration }}</td>
+                    <td>{{ $data1->Notes }}</td>
+                </tr>
+                @endforeach
+                @else
+                <tr>
+                    <td colspan="8">No data available</td>
+                </tr>
+                @endif
+            </tbody>
+        </table>
     </main>
-
-    <footer>
-
-    </footer>
-    @endsection 
+    @endsection
 </body>
+
 </html>
