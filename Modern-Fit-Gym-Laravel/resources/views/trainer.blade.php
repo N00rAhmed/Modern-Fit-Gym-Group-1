8fb639bc38571fc334f1da57afe5080ee4c72d94
Eliasali <elias02ali@hotmail.com>
2023-12-15 20:25:22 +0000

    sorted the footer and designed trainer page

--- Modern-Fit-Gym-Laravel/resources/views/trainer.blade.php
+++ Modern-Fit-Gym-Laravel/resources/views/trainer.blade.php
@@ -8,25 +8,20 @@
     <title>Trainer</title>
 </head>
 <body>
-    <header>
-        <nav>
-            <img class="navbar-logo" src="../Images/logo.png" height="100px" width="100px" />
-            <h2>Modern FIT Gym Trainer Page</h2>
-            <a href="/trainer">Trainer Home</a>
-            <a href="/viewDiary">View Diary</a>
-            <a href="/regime">Regime (crud functionality)</a>
-        </nav>
-    </header>
-
+      @extends('nav')
+      @section('content')
     <main>
         <h1>Welcome Trainer</h1>
 
-        <p>description for the trainer talkinf about saomthing generic in description. Maybe make font or text size of text bigger to make it look better</p>
+        <div class="cards">
+            <div class="card"><a href="/viewDiary">View diary</a></div> 
+            <div class="card"><a href="/regime">View Regime</a></div> 
+        </div>
 
     </main>
 
-    <footer>
-
-    </footer>
+    </main>
+    @include('footer')
+    @endsection
 </body>
 </html>
